7847291e6e536a6180feea8815b36ad96efed2ee
mperezg-the-goat <m.perez.g@ikasle.eus>
2026-09-21 18:01:10 +0200

    Delete soberako directory

--- 1. Ebaluazioa/11.ariketa.php
+++
@@ -1,8 +0,0 @@
-<?php
-$zbk=5;
-for($i=1;$i<=10;$i++){
-    $biderketa=$zbk*$i;
-    echo"$zbk X $i = $biderketa ";
-    echo"<br/>";
-}
-?>
--- 1. Ebaluazioa/25.ariketa.php
+++
@@ -1,4 +0,0 @@
-<?php
-$izenak=array('a'=>'amanda','b'=>'beronica','c'=>'carla','d'=>'dani','e'=>'ekaitz','f'=>'fabian');
-var_dump($izenak);
-?>
